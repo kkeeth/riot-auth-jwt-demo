d214788875b651c2f730d95d94023bed129bc96b
kkeeth <zensin0082@gmail.com>
2020-06-09 17:14:23 +0900

    feat: base routing for each pages

---
+++ libs/store.js
@@ -0,0 +1,6 @@
+import riotStx from 'riot-stx'
+
+// initialize state
+const initState = { auth: null }
+riotStx.initState(initState)
+
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@riotjs/hot-reload'
 import { component, register } from 'riot'
 import { Router, Route } from '@riotjs/route'
 import App from './app.riot'
+import '../libs/store'
 
 // register Router and Route components are globally
 register('router', Router)
