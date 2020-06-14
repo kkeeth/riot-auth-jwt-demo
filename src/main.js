f2ddd3103e94eb58d6e459e9f409fde73fd4fbe5
kkeeth <zensin0082@gmail.com>
2020-06-14 19:31:46 +0900

    feat: developed authentication logic with js-cookie

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import '@riotjs/hot-reload'
-import { component, register } from 'riot'
-import { Router, Route } from '@riotjs/route'
+import { component, register, install } from 'riot'
+import { Router, Route, router } from '@riotjs/route'
 import App from './app.riot'
 import '../libs/store'
 
 // register Router and Route components are globally
 register('router', Router)
 register('route', Route)
+
+install(component => {
+  component.router = router
+  return component
+})
 
 component(App)(document.getElementById('app'), {
   title: 'Hello Riot.js!'
